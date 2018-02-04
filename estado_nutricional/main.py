16456efa93a8fb996d6a60ac1d6399c33cbe01c2
Valery Briz <valerybriz@gmail.com>
2018-02-04 16:01:47 -0600

    initializing variables

--- estado_nutricional/main.py
+++ estado_nutricional/main.py
@@ -1,4 +1,6 @@
-"""Este codigo toma 4 variables: 
+#!/usr/bin/env python
+
+"""Este codigo toma 4 variables:
 - sexo
 - fecha de nacimiento
 - peso
@@ -12,3 +14,26 @@ y da como resultado los siguientes indices:
 en base a las tablas de referencia:
 http://www.who.int/childgrowth/standards/en/
 """
+
+import os
+import numpy as np
+
+class Growth(object):
+    def __init__(self, indicator, measurement, age_in_months, sex,
+                 height, american):
+        self.age = float(age_in_months)
+        self.sex = sex.upper()
+        self.height = float(height)
+        self.indicator = indicator
+        self.measurement = measurement
+        self.position = None
+        self.american = american
+
+        self.table_indicator = None
+        self.table_age = None
+        self.table_sex = None
+
+def age_in_weeks(self):
+    return ((self.age * 30.4374) / 7.0)
+
+
